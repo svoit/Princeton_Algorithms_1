7fc8b109fd9dd3b5b4289337a2510718b322da99
svoit <1726513+svoit@users.noreply.github.com>
2018-01-07 00:33:52 +0100

    Week 1: Percolation

---
+++ assignment_1/PercolationStats.java
@@ -0,0 +1,82 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+/**
+ * The class with main method to perform multiple percolation computation experiments.
+ *
+ * @author svoit
+ */
+public class PercolationStats {
+
+    private final double mean;
+    private final double stddev;
+    private final double confidenceLo;
+    private final double confidenceHi;
+
+    private final double[] experimentScores;
+
+    public PercolationStats(int n, int trials) {
+
+        if (n <= 0) {
+            throw new IllegalArgumentException("The grid size must be bigger than zero");
+        }
+
+        if (trials <= 0) {
+            throw new IllegalArgumentException("The number of experiments must be bigger than zero");
+        }
+
+        experimentScores = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            Percolation percolation = new Percolation(n);
+
+            int runs = 0;
+            while (!percolation.percolates()) {
+                int row, col;
+
+                do {
+                  col = 1 + StdRandom.uniform(n);
+                  row = 1 + StdRandom.uniform(n);
+                } while (percolation.isOpen(row, col));
+
+                percolation.open(row, col);
+                runs++;
+            }
+
+            experimentScores[i] = runs / (double) (n * n);
+        }
+
+        // avoiding extra calls
+        mean = StdStats.mean(experimentScores);
+        stddev = StdStats.stddev(experimentScores);
+        double confidenceFraction = (1.96 * stddev()) / Math.sqrt(experimentScores.length);
+        confidenceLo = mean - confidenceFraction;
+        confidenceHi = mean + confidenceFraction;
+    }
+
+    public double mean() {
+        return mean;
+    }
+
+    public double stddev() {
+        return stddev;
+    }
+
+    public double confidenceLo() {
+        return confidenceLo;
+    }
+
+    public double confidenceHi() {
+        return confidenceHi;
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+        PercolationStats percolationStats = new PercolationStats(n, t);
+
+        StdOut.printf("mean                    = %f\n", percolationStats.mean());
+        StdOut.printf("stddev                  = %f\n", percolationStats.stddev());
+        StdOut.println("95% confidence interval = " + percolationStats.confidenceLo() + ", " + percolationStats.confidenceHi());
+    }
+}
